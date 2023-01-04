c55238d0f3c16452cfe1bbfadc6597d45d1b313e
YAHYA KARA <sukurcan61@gmail.com>
2023-01-04 20:55:13 +0300

    bugs fixed

--- ELM327_PID_DataCollector/Program.cs
+++ ELM327_PID_DataCollector/Program.cs
@@ -12,7 +12,6 @@ namespace ELM327_PID_DataCollector
         static void Main(string[] args)
         {
             Console.WriteLine("\r\n  ___  _     __  __  ____ ___  ____  __      __ _  ___  _      _  _  ___  _____ \r\n | __|| |   |  \\/  ||__ /|_  )|__  | \\ \\    / /(_)| __|(_)    | \\| || __||_   _|\r\n | _| | |__ | |\\/| | |_ \\ / /   / /   \\ \\/\\/ / | || _| | |  _ | .` || _|   | |  \r\n |___||____||_|  |_||___//___| /_/     \\_/\\_/  |_||_|  |_| (_)|_|\\_||___|  |_|  \r\n                                                                                \r\n");
-            Console.WriteLine("Contact : sukurcan61@gmail.com");
             Console.WriteLine();
             Console.Write("Enter OBD Device IP address (ex:192.168.0.10): ");
             var Ip = Console.ReadLine();
